ecf64b6a1d51a39cb28673df4ee2d8fe9c0539b8
Sergey Kiselev <sk.mails@yandex.ru>
2023-05-05 18:57:35 +0300

    fit: Реализовано обновление данных пользователя на странице и сервере

---
+++ src/components/EditProfilePopup.js
@@ -0,0 +1,71 @@
+import { useState, useContext, useEffect } from 'react';
+import { CurrentUserContext } from '../context/CurrentUserContext.js';
+import PopupWithForm from './PopupWithForm.js';
+
+function EditProfilePopup({ isOpen, onClose, onSubmitPopup }) {
+
+  const currentUser = useContext(CurrentUserContext);
+  const [valueName, setValueName] = useState('');
+  const [valueDescription, setValueDescription] = useState('');
+
+  useEffect(() => {
+    setValueName(currentUser.name || '');
+    setValueDescription(currentUser.about || '');
+  }, [currentUser])
+
+  function onNameChange(evt) {
+    setValueName(evt.target.value);
+  }
+
+  function onDescriptionChange(evt) {
+    setValueDescription(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onSubmitPopup(valueName, valueDescription);
+  }
+
+  return (
+    <PopupWithForm
+      name="editForm"
+      title="Редактировать профиль"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmitPopup={handleSubmit}
+    >
+      <fieldset className="popup__fieldset">
+        <label>
+          <input
+            value={valueName}
+            onChange={onNameChange}
+            className="popup__input popup__input_type_editForm-name"
+            id="name-input"
+            type="text"
+            name="name"
+            placeholder="Введите свое имя"
+            required
+            maxLength="40"
+            minLength="2"/>
+          <span className="popup__input-error name-input-error"/>
+        </label>
+        <label>
+          <input
+            value={valueDescription}
+            onChange={onDescriptionChange}
+            className="popup__input popup__input_type_editForm-description"
+            id="description-input"
+            type="text"
+            name="about"
+            placeholder="Введите свой род занятий"
+            required
+            maxLength="200"
+            minLength="2"/>
+          <span className="popup__input-error description-input-error"/>
+        </label>
+      </fieldset>
+    </PopupWithForm>
+  );
+}
+
+export default EditProfilePopup;
